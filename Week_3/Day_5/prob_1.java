781e1505bcc5dc3443d3e2c939b863f99a305aa0
Aryan <225872042+aryan24ui@users.noreply.github.com>
2026-06-19 21:29:25 +0530

    Implement dailyTemperatures method in Solution class

---
+++ Week_3/Day_5/prob_1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> s = new Stack<>();
+        int answer[] = new int[temperatures.length];
+ 
+        for(int i=0; i<temperatures.length; i++) {
+            while(!s.isEmpty() && temperatures[i] > temperatures[s.peek()]) {
+                int idx = s.pop();
+                answer[idx] = i - idx;
+            }
+            s.push(i);
+        }
+        return answer;
+    }
+}
